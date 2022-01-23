280d7b0a5c930af6de83598b1a2284d476f9f255
TDolbow <tdolbow@udel.edu>
2022-01-23 10:37:50 -0500

    Added functionality to allow JSON to be read and placed into extension text.

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -1,8 +1,71 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var checkPageButton = document.getElementById('checkPage');
-    checkPageButton.addEventListener('click', function() {
-        document.getElementById('Header1').innerText = "Button was clicked";
-        
+document.addEventListener('DOMContentLoaded', function () {
+    var checkPageButton = document.getElementById('upvote');
+    checkPageButton.addEventListener('click', function () {
+        document.getElementById('title').innerText = "Button was clicked";
+        populatePage(testJSON);
+
 
     }, false);
-  }, false);
+}, false);
+
+
+
+let testJSON = JSON.stringify({
+    siteRating: 54,
+    pageRating: 45,
+    comments: [
+        {
+            UUID: 'ID1',
+            comment: 'some text here'
+        },
+        {
+            UUID: 'ID2',
+            comment: 'some more text'
+        },
+        {
+            UUID: 'ID3',
+            comment: 'this site sucks'
+        }
+    ]
+});
+
+//testJSON = JSON.stringify({siteRating : 54, pageRating : 45});
+
+function populatePage(json) {
+    let parsed = JSON.parse(json);
+    
+    document.getElementById("siteRating").innerText = parsed.siteRating;
+    document.getElementById("pageRating").innerText =  parsed.pageRating;
+    
+    
+    commentArr = parsed.comments;
+    
+
+    for(let i = 0; i < commentArr.length; i++){
+        let table = document.getElementById("comments")
+        let row = document.createElement('tr'); 
+        let column1 = document.createElement('td');
+        let column2 =document.createElement('td');
+
+        column1.appendChild(document.createTextNode(commentArr[i].UUID));
+        column2.appendChild(document.createTextNode(commentArr[i].comment));
+
+        row.appendChild(column1);
+        row.appendChild(column2);
+        
+        //row.appendChild(document.createTextNode(commentArr[i].comment));
+
+        table.appendChild(row);
+
+    }
+
+
+
+
+
+
+
+
+
+
+}
