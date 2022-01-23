b6ae289c59d428564508c6cc9b723e8154672482
TDolbow <tdolbow@udel.edu>
2022-01-23 15:43:55 -0500

    Added http

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -24,7 +24,7 @@ function getUserID(){ // Currently sets title to saved user ID.
 function newUser(){ //Function to get new uesr ID. 
     data = {};
     
-    fetch("192.168.64.3:8000/api/new-user",{
+    fetch("http://192.168.64.3:8000/api/new-user",{
         method : "GET",
         headers: {'Content-Type': 'application/json'}
         }).then(res => {
@@ -37,10 +37,10 @@ function newUser(){ //Function to get new uesr ID.
 
 async function getSiteStats(domain, url){ // Gets specified domain and url site stats from backend and populates the page
     data = {domain : domain, url : url};
-    fetch("192.168.64.3:8000/api/site-stats", {
+    fetch("http://192.168.64.3:8000/api/site-stats", {
         method : "GET",
         headers: {'Content-Type': 'application/json'},
-        body: JSON.stringify(data)
+        //body: JSON.stringify(data)
     }).then(res => {
         populatePage(res) //Populates page with results. 
         
@@ -50,7 +50,7 @@ async function getSiteStats(domain, url){ // Gets specified domain and url site 
 
 function sendRating(UID, rating, domain, url){ //Sends the user's rating to the backend based on the specific domain and url page.  
     data = {domain: domain, url: url, UID : UID, rating: rating};
-    fetch("192.168.64.3:8000/api/rating", {
+    fetch("http://192.168.64.3:8000/api/rating", {
         method : "POST",
         headers: {'Content-Type': 'application/json'},
         body: JSON.stringify(data)
@@ -62,7 +62,7 @@ function sendRating(UID, rating, domain, url){ //Sends the user's rating to the 
 
 function sendComment(UID, comment, domain, url){ //Sends the user's comment to the backend based on the specific domain and url. 
     data = {};
-    fetch("192.168.64.3:8000/api/comment", {
+    fetch("http://192.168.64.3:8000/api/comment", {
         method : "POST",
         headers: {'Content-Type': 'application/json'},
         body: JSON.stringify(data)
